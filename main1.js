37f3621e8efbcd0fa809b53929291bc6871ed00d
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-01-12 16:57:01 +0100

    svolto selfwork operatori

---
+++ main1.js
@@ -0,0 +1,12 @@
+let a = 34;
+let b = 84;
+let c = 79;
+let d = 25;
+let e = 62;
+
+console.log(`la somma tra in numeri equivale a: ${a + b + c + d + e }`);
+
+let tot = 284;
+
+console.log(`la media equivale a: ${tot / 5}`);
+
