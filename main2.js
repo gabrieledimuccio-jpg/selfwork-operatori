c429cbb3f0243e20a76081fce78848b520e2201f
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-01-13 11:36:40 +0100

    svolto esercizio operatori 2

---
+++ main2.js
@@ -0,0 +1,11 @@
+let anno = 2026;
+let pippo = 2000;
+
+let anni = anno - pippo;
+console.log(anni);
+
+let secolo = 2100;
+let cent = secolo - anno;
+console.log(cent);
+
+console.log(`Pippo ha ${anni} anni, e gli mancano ${cent} anni per compierne 100.`);
